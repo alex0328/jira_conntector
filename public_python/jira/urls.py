3f70bc68236431ae8d9bf35a9d08ab741cdb0e7e
Lukasz Szlaszynski <lukaszszlaszynski@MBP-Lukasz.lan>
2023-02-17 10:06:35 +0100

    initial commit

---
+++ public_python/jira/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, re_path
+from jira import views
+
+app_name = 'jira'
+urlpatterns = [
+    # path('', views.Index.as_view(), name='index'),
+    # path('fun', views.Fun.as_view(), name='fun'),
+    ]
